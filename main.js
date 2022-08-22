aa00465236c1367af25bfcd279ba78fd02240da0
Raul Rodrigues <raulrm.dev@gmail.com>
2022-08-22 20:06:03 -0300

    include minute count

--- main.js
+++ main.js
@@ -1,8 +1,10 @@
 window.onload = function () {
+    var minutes = 00;
     var seconds = 00; 
     var tens = 00; 
     var appendTens = document.getElementById("tens")
     var appendSeconds = document.getElementById("seconds")
+    var appendMinutes = document.getElementById("minutes")
     var buttonStart = document.getElementById('button-start');
     var buttonStop = document.getElementById('button-stop');
     var buttonReset = document.getElementById('button-reset');
@@ -22,8 +24,10 @@ window.onload = function () {
         clearInterval(Interval);
         tens = "00";
         seconds = "00";
+        minutes = "00";
         appendTens.innerHTML = tens;
         appendSeconds.innerHTML = seconds;
+        appendMinutes.innerHTML = minutes;
     }
 
     function startTimer () {
@@ -44,9 +48,23 @@ window.onload = function () {
           tens = 0;
           appendTens.innerHTML = "0" + 0;
         }
-        
-        if (seconds > 9){
+
+        if (seconds > 9) {
           appendSeconds.innerHTML = seconds;
-        }      
-      }    
+        }
+
+        if (seconds > 59) {
+          console.log("minutes");
+          minutes++;
+          appendMinutes.innerHTML = "0" + minutes;
+          seconds = 0;
+          tens = 0;
+          appendSeconds.innerHTML = "0" + 0;
+          appendTens.innerHTML = "0" + 0;
+        }
+        
+        if (minutes > 9){
+          appendMinutes.innerHTML = minutes;
+        } 
+      }
     }
